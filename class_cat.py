092d2b58af5c89292e2006b2fe63aa7725f55469
Weiwei Lin <stormer.boxes-06@icloud.com>
2022-03-24 21:20:29 -0400

    11

--- class_cat.py
+++ class_cat.py
@@ -15,30 +15,21 @@ class Cat:
     def display_color(self):
         print("my cat color is " + self.color)
 
+    def cat_play(self):
+        print(f"cat {self.name} is very happy now.")
+
 
 Cat_a = Cat("tony", 3, "orange")
 Cat_a.display_name()
 Cat_a.display_age()
 Cat_a.display_color()
+Cat_a.cat_play()
 
 
-class Restaurant:
-    def __init__(self, name, style, business_time):
-        self.name = name
-        self.style = style
-        self.business_time = business_time
-
-    def show_name(self):
-        print("餐厅的名字是" + self.name)
-
-    def show_style(self):
-        print("餐厅的主营系是" + self.style)
-
-    def show_business_time(self):
-        print("餐厅在" + self.business_time)
+def week_pay(hour_pay, hour):
+    salary = hour_pay * hour
+    return salary
 
 
-Restaurant_duck = Restaurant("全聚德", "烤鸭", "营业中")
-Restaurant_duck.show_name()
-Restaurant_duck.show_style()
-Restaurant_duck.show_business_time()
+Emplyee_salary_Mike = week_pay(20.29, 40)
+print(f"your weekpay is {Emplyee_salary_Mike:.2f}")
---
+++ order_food.py
@@ -0,0 +1,36 @@
+print("""Menu:
+1,Salad  $5.99
+2,Soup  $6.95
+3,Pizza  $10.59
+4,Burger  $8.66
+""")
+menu = {"Salad": 5.99, "Soup": 6.95, "Pizza": 10.59, "Burger": 8.66, "Beer": 7.99}
+
+
+order = []
+while len(order) == 0 or input("Input “finish” when you finish your order.") != "finish":
+    dishes = ""
+    while dishes not in menu.keys():
+        try:
+            dishes = input("Please input the dishes name!")
+        except ValueError:
+            print("You only can input the dishes name!")
+
+    quantity = 0
+    while quantity not in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+        try:
+            quantity = int(input("""Please input your dishes quantity. The max quantity is 10!"""))
+        except ValueError:
+            print("You only can input the quantity number!")
+    order.append((dishes, quantity))
+print("I finish order!")
+
+
+def total(order):
+    total_price = 0
+    for (dishes, quantity) in order:
+        total_price = total_price + menu[dishes] * quantity
+    return total_price
+
+
+print(total(order))
