79cac348805dd410eaf873e2dbdca558b6bfaf32
Weiwei Lin <stormer.boxes-06@icloud.com>
2022-04-11 20:07:53 -0400

    111

--- Day15_coffee.py
+++ Day15_coffee.py
@@ -30,9 +30,10 @@ resources = {
     "coffee": 100,
 }
 
+
 def is_resource_sufficient(order_ingredients):
     for item in order_ingredients:
-        if order_ingredients[item] >= resources[item]
+        if order_ingredients[item] >= resources[item]:
             print(f"Sorry there is not enough {item}.")
             return False
     return True
@@ -47,9 +48,25 @@ def process_coins():
     return totol
 
 
+def is_transaction_successful(money_received, drink_cost):
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is ${change} in change.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+
+
+def make_coffee(drink_name, order_ingredients):
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name}coffee ☕️ enjoy！")
+
 
 is_on = True
-
 while is_on:
     choice = input("What would you like?(espresso/latte/cappuccino):")
     if choice == "off":
@@ -63,4 +80,5 @@ while is_on:
         drink = MENU[choice]
         if is_resource_sufficient(drink["ingredients"]):
             payment = process_coins()
-
+            if is_transaction_successful(payment, drink["cost"]):
+                make_coffee(choice, drink["ingredients"])
