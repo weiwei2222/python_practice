f24b4c3576fe79d562cb74ba5c4f5be162d28d97
Weiwei Lin <stormer.boxes-06@icloud.com>
2022-04-12 09:39:54 -0400

    111

--- Day17_main.py
+++ Day17_main.py
@@ -1,5 +1,6 @@
 from Day17_question_model import Question
-from Day17_data import  question_data
+from Day17_data import question_data
+from Day17_quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
@@ -7,3 +8,9 @@ for question in question_data:
     question_answer = question["answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
