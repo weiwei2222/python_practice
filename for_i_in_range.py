5d89795f7e90b298852dd9c86273b9585ad0984e
Weiwei Lin <stormer.boxes-06@icloud.com>
2022-04-15 20:15:40 -0400

    111

--- for_i_in_range.py
+++ for_i_in_range.py
@@ -2,8 +2,12 @@
 # 程序分析：可填在百位、十位、个位的数字都是1、2、3、4。组成所有的排列后再去 掉不满足条件的排列。
 
 
-for i in range(1,5):
-    for j in range(1,5):
-        for k in range(1,5):
-            if(i !=k) and (i !=j) and (j !=k):
-                print (i, j, k)
+# for i in range(1, 5):
+#     for j in range(1, 5):
+#         for k in range(1, 5):
+#             if (i != k) and (i != j) and (j != k):
+#                 print(i, j, k)
+
+my_list = [1, 2, 3, 4]
+for my_list in range(4, 0, -1):
+    print(my_list)
