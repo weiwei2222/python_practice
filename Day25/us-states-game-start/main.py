e665368e76cbfafa1c9fdf66fe5c73204e07f033
Weiwei Lin <stormer.boxes-06@icloud.com>
2022-04-20 19:22:39 -0400

    111

--- Day25/us-states-game-start/main.py
+++ Day25/us-states-game-start/main.py
@@ -2,10 +2,34 @@ import turtle
 import pandas
 
 screen = turtle.Screen()
+screen.setup(width=725, height=491)
 screen.title("U.S States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
-
 turtle.shape(image)
 
-screen.exitonclick()
+state_data = pandas.read_csv("50_states.csv")
+all_states = state_data.state.to_list()
+
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        missing_state = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_state.append(state)
+        new_data = pandas.DataFrame(missing_state)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        us_data = state_data[state_data.state == answer_state]
+        t.goto(int(us_data.x), int(us_data.y))
+        t.write(us_data.state.item())
+
